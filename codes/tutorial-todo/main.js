ee16efafbf2034a9bf5c3d2625d0831237055bb1
“junya-ori” <junya.habu@orithankyou.co.jp>
2020-05-16 14:50:26 +0900

    テンプレート作成

--- codes/tutorial-todo/main.js
+++ codes/tutorial-todo/main.js
@@ -1,20 +1,36 @@
-
-// データをローカルストレージに保存
-
-
 // Vue.jsの記述
 new Vue({
   el: '#app',
   data: {
-    userName: "hoge"
-  },
-  watch: {
-  },
-  computed: {
-  },
-  created() {
+    name: "",
+    activities: [{date: "12/20", section: "定例", time: "5", detail: "ほげほげ"}, {date: "12/25", section: "コアMTG", time: "5", detail: "ほげほげ"}]
   },
   methods: {
+    addName: function() {
+      // ref で名前を付けておいた要素を参照
+      this.name = this.$refs.name.value;
+      // フォーム要素を空にする
+      this.$refs.name.value = "";
+    },
+    addActivity: function() {
+      // ref で名前を付けておいた要素を参照
+      var date = this.$refs.date;
+      var section = this.$refs.section;
+      var time = this.$refs.time;
+      var detail = this.$refs.detail;
+
+      this.activities.push({
+        date: date.value,
+        section: section.value,
+        time: time.value,
+        detail: detail.value,
+      });
+      // フォーム要素を空にする
+      date.value = "";
+      section.value = "";
+      time.value = "";
+      detail.value = "";      
+    }
   },
 });
 
